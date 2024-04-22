abff969106f153c076d6cf47b62e1bf8f8a73827
Akachukwu <egbolucheakachukwu@gmail.com>
2024-04-22 22:41:46 +0100

    Probably Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Header from './Components/Header'
 import Hero from './Components/Hero'
 import Marquee from './Components/Marquee'
 import About from './Components/About'
+import WhyChoose from './Components/WhyChoose'
+import Services from './Components/Services'
+import Community from './Components/Community'
+import TeamSection from './Components/TeamSection'
+import Newsletter from './Components/Newsletter'
+import Faq from './Components/Faq'
+import Footer from './Components/Footer'
 
 function App() {
 
@@ -11,6 +18,13 @@ function App() {
       <Hero />
       <Marquee />
       <About />
+      <WhyChoose />
+      <Services />
+      <Community />
+      <TeamSection />
+      <Newsletter />
+      <Faq />
+      <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import ServicesPage from './Components/ServicesPage.jsx'
+import Contact from './Components/Contact.jsx'
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/services", element: <ServicesPage /> },
+  { path: "/contact", element: <Contact /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
